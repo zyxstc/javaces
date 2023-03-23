6c9bd1136be870c441bb6df6cbdd6db08f8395db
zyxstc <56866606+zyxstc@users.noreply.github.com>
2023-03-23 20:53:20 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,37 @@
+body = {
+{
+    "code": "0",
+    "message": "success",
+    "datas": {
+        "totalSize": 1,
+        "pageSize": 20,
+        "pageNumber": 1,
+        "allowApply": 0,
+        "allowPcApply": 1,
+        "rows": [{
+            "id": "170dd558294442c784ec4c17355a5b34",
+            "leaveType": "事假",
+            "createTime": "10-20 16:47",
+            "startTime": "10-21 08:00",
+            "endTime": "10-21 20:00",
+            "totalDay": 1,
+            "status": "6",
+            "expireDay": 2,
+            "isExtend": "1",
+            "applyTime": "10-20 16:47",
+            "out": "1",
+            "leaveTime": "12小时",
+            "startTimePC": "10-21 08:00",
+            "endTimePC": "10-21 20:00",
+            "leaveReason": "外出办理身份证 望辅导员批准",
+            "shutDown": false,
+            "actEndTime": "10-21 20:00",
+            "actEndTimeDesc": "12小时",
+            "actStatus": "6",
+            "isEarlyEnd": "0"
+        }]
+    }
+}
+}
 $notify("123", "456", "789");
-$done()
+$done( body:body)
