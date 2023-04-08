0d011291d7164ab5a4a3f6fcd543a040d6b12de6
zyxstc <56866606+zyxstc@users.noreply.github.com>
2023-04-09 01:28:30 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,34 +1,22 @@
-body = {
-    "code": "0",
-    "message": "success",
-    "datas": {
-        "totalSize": 1,
-        "pageSize": 20,
-        "pageNumber": 1,
-        "allowApply": 0,
-        "allowPcApply": 1,
-        "rows": [{
-            "id": "170dd558294442c784ec4c17355a5b34",
-            "leaveType": "事假",
-            "createTime": "10-20 16:47",
-            "startTime": "10-21 08:00",
-            "endTime": "10-21 20:00",
-            "totalDay": 1,
-            "status": "6",
-            "expireDay": 2,
-            "isExtend": "1",
-            "applyTime": "10-20 16:47",
-            "out": "1",
-            "leaveTime": "12小时",
-            "startTimePC": "10-21 08:00",
-            "endTimePC": "10-21 20:00",
-            "leaveReason": "外出办理身份证 望辅导员批准",
-            "shutDown": false,
-            "actEndTime": "10-21 20:00",
-            "actEndTimeDesc": "12小时",
-            "actStatus": "6",
-            "isEarlyEnd": "0"
-        }]
-}
-}
-$done({body:JSON.stringify(body)});
+const url = "https://api.day.app/ioLQp3P6bsyn6cMpWzzptQ/这里改成你自己的推送内容";
+const method = "POST";
+const headers = {"Field": "test-header-param"};
+const data = {"info": "abc"};
+
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("Title", "Subtitle", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+    $done();
+});
