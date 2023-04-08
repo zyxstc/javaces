d5ee562492cbcda96c7638b9529ec6153672efaf
zyxstc <56866606+zyxstc@users.noreply.github.com>
2023-04-09 01:33:03 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-const url = "https://api.day.app/ioLQp3P6bsyn6cMpWzzptQ/123123123";
+const url = "https://api.day.app/ioLQp3P6bsyn6cMpWzzptQ/44444";
 const method = "GET";
 const headers = {"Field": "test-header-param"};
 
